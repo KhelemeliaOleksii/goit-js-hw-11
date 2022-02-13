9026a0f1ea58a8a5df55de4850237363145fce12
KhelemeliaOleksii <xvdm@ukr.net>
2022-02-13 18:24:22 +0200

    search form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import './sass/main.scss';
 //import { print } from './js/api/fetch';
 
-import { searchForm } from './js/search-form'
+import {searchForm } from './js/search-form'
 
 
 const emptyRequestMessage = "Sorry, there are no images matching your search query. Please try again.";
 
 
-
-searchForm.form.addEventListener('submit', searchForm.submitListener);
+const searchPhoto = new searchForm();
 
 
